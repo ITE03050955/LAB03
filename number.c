4d3b8fbd6a0670db42472190a7dcc4895832b4e1
ITE03050955 <b24875g@gmail.com>
2016-09-19 17:05:42 +0800

    first commit number

---
+++ number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+	int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
+	printf("請輸入十個數字,用空格隔開\n");
+	scanf("%d %d %d %d %d %d %d %d %d %d",&n1,&n2, &n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10);
+	printf("您輸入的數字為:");
+	printf("\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n1,n2,n3,n4,n5,n6,n7,n8,n9,n10);
+	
+	return 0;
+	}
